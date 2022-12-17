c6d057331bcd4a8506afeb228a387c1ea484a503
Emily Weagraff <weagraff.emily@gmail.com>
2022-12-17 14:52:37 -0800

    iterables

---
+++ iterableArrays.js
@@ -0,0 +1,7 @@
+var fruits = ["Mango", "Banana", "Pineapple", "Kiwi"]
+
+console.log(fruits.length)
+
+for(i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+}
---
+++ iterableStrings.js
@@ -0,0 +1,7 @@
+var greeting = "hello"
+
+console.log(greeting.length)
+
+for(i = 0; i < greeting.length; i++){
+    console.log(greeting[i])
+}
