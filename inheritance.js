12fc7f2934052ec82efc3aec2f16c345a5c418b4
Emily Weagraff <weagraff.emily@gmail.com>
2022-12-29 18:01:48 -0800

    inheritance practice

---
+++ inheritance.js
@@ -0,0 +1,21 @@
+var bird = {
+    hasWings: true,
+    canFly: true,
+    hasFeathers: true
+}
+
+var parrot1 = Object.create(bird)
+console.log("parrot1", parrot1);
+console.log("parrot1 has wings:", parrot1.hasWings)
+
+parrot1 = {
+   hasWings: true,
+   canFly: true,
+   hasFeathers: false
+}
+
+console.log(parrot1)
+
+//inheritance of object properties can happen when you set a prototype that multiple objects 
+// can inherit those properties. 
+
