1717c04cab878924f5a241e878c9b0addcf4b920
Emily Weagraff <weagraff.emily@gmail.com>
2023-01-09 13:36:18 -0800

    template literals practice

---
+++ templateLiterals.js
@@ -0,0 +1,19 @@
+// ES6 Multi-line template literals 
+// template literals require backtick
+
+let multiLine = `
+Using ES6
+backticks,
+multi-line
+strings are
+possible!
+`;
+
+console.log(multiLine)
+
+//variable interpolation using concatination 
+
+let first = "Hi My name is Emily"
+let second = "I was born in Anchorage, Alaska"
+
+console.log(`${first} + ${second}`)
