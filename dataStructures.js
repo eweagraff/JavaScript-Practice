bea95e05af2e2b5c411451b3a9f2fd7d0671aa02
Emily Weagraff <weagraff.emily@gmail.com>
2023-01-12 13:36:34 -0800

    data structures

---
+++ dataStructures.js
@@ -0,0 +1,20 @@
+//Maps
+
+//Array
+/* 
+Methods that are specific for an array that allow you to loop over.
+   1. forEach
+   2. filter
+   3. map
+
+*/
+const fruits = ['kiwi', 'mango', 'apple', 'pear'];
+function appendIndex(fruit, index){
+    console.log(`${index}. ${fruit}`)
+}
+fruits.forEach(appendIndex);
+
+
+//Map
+
+//Set Data
