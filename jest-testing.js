32b7ee70aeacbca43225839c82933b98d3b4b200
Emily Weagraff <weagraff.emily@gmail.com>
2023-01-23 13:14:28 -0800

    receipt maker, little lemon restaurant

--- jest-testing.js
+++ jest-testing.js
@@ -15,3 +15,6 @@ const timesTwo = require('./timesTwo');
 test('returns the number times 2', () => {
     expect(timesTwo(10)).toBe(20)
 });
+
+
+//Unit tests are used to make sure a program or webpage is running efficiently. You can make changes based on unit tests or refactor your code.
